e1dbdd2062ea93792450392fa724cbd3583b7564
devofficer <myeyesontech@gmail.com>
2023-02-02 07:36:29 -0800

    [fix] changed sidebar to navbar and made a new sidebar

--- pages/board/index.js
+++ pages/board/index.js
@@ -1,10 +1,12 @@
 import styles from './style.module.css';
+import { Navbar } from '../../components/navbar';
 import { Sidebar } from '../../components/sidebar';
 
 export default function Board() {
   return (
     <div className={styles.layout}>
-      <Sidebar />
+      <Navbar />
+      <Sidebar notifications={10}/>
     </div>
   );
 }
